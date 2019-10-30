281d7940c2ee2e936bd4f662ec40c0ed57b1b999
DManikyam <dmanikyam@traveltripper.com>
2019-10-30 12:22:54 +0530

    Instagram Slider implemented

---
+++ _jssrc/app.js
@@ -0,0 +1,37 @@
+function readyDoc(fn) {
+ var d = document;
+ (d.readyState == 'loading') ? d.addEventListener('DOMContentLoaded', fn): fn();
+}
+
+readyDoc(function() {
+
+  setTimeout(function(){
+    //Instagram Slider
+    if (document.getElementsByClassName('insta-slider').length > 0) {
+      var slider = tns({
+        container: '.insta-slider',
+        items: 2,
+        nav: false,
+        mouseDrag: true,
+        loop: true,
+        prevButton: "#customNavItemsInsta .iconbtn--left", // previous button
+  			nextButton: "#customNavItemsInsta .iconbtn--right", // next button
+        responsive: {
+          640: {
+            items: 3
+          },
+          700: {
+            items: 4
+          },
+          900: {
+            items: 4
+          },
+          1200: {
+            items: 6
+          }
+        }
+      });
+    }
+  }, 2000);
+
+});
