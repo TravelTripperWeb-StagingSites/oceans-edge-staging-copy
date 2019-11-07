0ab9ec35d5efba6b6468a4ed095057e871e99290
Srinivas <rsrinivas13@gmail.com>
2019-11-07 15:52:33 +0530

    Offer details carousel updated

--- _jssrc/main.bundle.js
+++ _jssrc/main.bundle.js
@@ -45,7 +45,8 @@ readyDoc(function () {
         nextButton: "#offersCarouselNav .iconbtn--right", // next button
         responsive: {
           767: {
-            items: 2
+            items: 2,
+            "autoHeight": false
           },
           1199: {
             items: 3
