5753dcea694796c6614faf8f8078837a86508ede
DManikyam <dmanikyam@traveltripper.com>
2019-11-19 19:24:49 +0530

    Zoom feature

--- _jssrc/app.js
+++ _jssrc/app.js
@@ -818,12 +818,12 @@ readyDoc(function() {
     });
   }
 
-  document.querySelector('.rooms-cross-sell .icon-maximize').addEventListener('click', () => {
-    document.querySelector('.rooms-cross-sell .rooms-cross-sell__image').classList.add('fullscreen-carousel');
+  document.querySelector('.fullscreen-cross-sell .icon-maximize').addEventListener('click', () => {
+    document.querySelector('.fullscreen-cross-sell').classList.add('fullscreen-carousel');
   });
 
-  document.querySelector('.rooms-cross-sell .zoomout').addEventListener('click', () => {
-    document.querySelector('.rooms-cross-sell .rooms-cross-sell__image').classList.remove('fullscreen-carousel');
+  document.querySelector('.fullscreen-cross-sell .zoomout').addEventListener('click', () => {
+    document.querySelector('.fullscreen-cross-sell').classList.remove('fullscreen-carousel');
   });
 
 });
